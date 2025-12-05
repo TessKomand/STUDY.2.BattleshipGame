79e2c5b2898a4265ae97ef824f5285c10ae4f4cb
TessKomand <minioglomski@gmail.com>
2025-12-05 17:26:05 +0100

    wip historia

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -7,36 +7,46 @@
             int pozycjay;
             int strzaly = 0;
             int wynik = 0;
-            for (int i = 0; i < 10; i++) {
-                pozycjax = rnd.Next(0, 10);
-                pozycjay = rnd.Next(0, 10);
-                if (mapa[pozycjax, pozycjay] == 0) {
-                    mapa[pozycjax, pozycjay] = 1;
-                } else {
-                    i--;
-                }
-            }
-            Console.WriteLine("Witaj w grze w statki, grasz na mapie 10 na 10, musisz zestrzelić 5 statków. Podaj swoje miejsce strzelania, masz 10 strzałów. podawaj miejsce jako x y");
-            do {
-                string[] misjece = Console.ReadLine().Split(' ');
-                int x = Convert.ToInt32(misjece[0]) - 1;
-                int y = Convert.ToInt32(misjece[1]) - 1;
-                if (x < 0 || x > 9 || y < 0 || y > 9) {
-                    Console.WriteLine("Podano złe współrzędne, podaj jeszcze raz");
-                    continue;
-                }
-                if (mapa[x, y] == 1) {
-                    Console.WriteLine("Trafiony zatopiony!");
-                    mapa[x, y] = 0;
-                    wynik++;
-                } else {
-                    Console.WriteLine("Pudło!");
-                }
-                strzaly++;
-                Sonar(mapa,x, y);
-            } while (strzaly <= 20);
-            Koniec(wynik);
+            List<string> historia = new List<string>();
+            Console.WriteLine("Witaj w grze w Statki, wpisz 1 by zacząć, 2 dla historii, 3 by zakończyć");
+            int decyzja = int.TryParse(Console.ReadLine(), out int d) ? d : -1;
 
+            do {
+                if (decyzja == 1) {
+                    for (int i = 0; i < 10; i++) {
+                        pozycjax = rnd.Next(0, 10);
+                        pozycjay = rnd.Next(0, 10);
+                        if (mapa[pozycjax, pozycjay] == 0) {
+                            mapa[pozycjax, pozycjay] = 1;
+                        } else {
+                            i--;
+                        }
+                    }
+                    Console.WriteLine("Witaj w grze w statki, grasz na mapie 10 na 10, musisz zestrzelić 5 statków. Podaj swoje miejsce strzelania, masz 10 strzałów. podawaj miejsce jako x y");
+                    do {
+                        string[] misjece = Console.ReadLine().Split(' ');
+                        int x = Convert.ToInt32(misjece[0]) - 1;
+                        int y = Convert.ToInt32(misjece[1]) - 1;
+                        if (x < 0 || x > 9 || y < 0 || y > 9) {
+                            Console.WriteLine("Podano złe współrzędne, podaj jeszcze raz");
+                            continue;
+                        }
+                        if (mapa[x, y] == 1) {
+                            Console.WriteLine("Trafiony zatopiony!");
+                            mapa[x, y] = 0;
+                            wynik++;
+                        } else {
+                            Console.WriteLine("Pudło!");
+                        }
+                        strzaly++;
+                        Sonar(mapa, x, y);
+                    } while (strzaly <= 20);
+                    Koniec(wynik);
+                }
+                if (decyzja == 2) {
+                    Historia();
+                }
+            } while (decyzja != 4);
 
         }
 
@@ -58,9 +68,10 @@
                 return 0;
             }
         }
-        static int Koniec(int wynik) {
+        static int Koniec(int wynik,List<string> hisoria) {
             if (wynik == 10) {
                 Console.WriteLine("Totalna wygrana");
+                hisoria.Add("Totalna wygrana");
             } else if (wynik == 7) {
                 Console.WriteLine("Wygrana");
             } else if (wynik == 4) {
@@ -68,8 +79,11 @@
             } else {
                 Console.WriteLine("Przegrana");
             }
-            Console.WriteLine("Wynik: "+wynik);
+            Console.WriteLine("Wynik: " + wynik);
             return 0;
+        }
+        static string Historia() {
+            return "";
         }
     }
 }
